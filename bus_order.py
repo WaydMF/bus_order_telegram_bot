6cf5f2897faee9f1a154d4e5a5eaf59ee3ce37e9
Alexander Shukelovich <shuk.san4@yandex.ru>
2020-04-08 22:31:06 +0300

    bus_order_telegram_bot_v0.1

---
+++ bus_order.py
@@ -0,0 +1,90 @@
+import logging
+import os
+import time
+
+import telebot
+
+from order import Order
+
+
+logger = telebot.logger
+telebot.logger.setLevel(logging.DEBUG)
+
+bot = telebot.TeleBot(os.getenv("TELEGRAM_BOT_API"))
+keyboard_cities = telebot.types.ReplyKeyboardMarkup(True, True)
+keyboard_cities.row("Минск", "Ошмяны")
+order = Order()
+
+
+@bot.message_handler(commands=["start"])
+def first_order_message(message):
+    msg = bot.send_message(message.chat.id, "Привет, выберите город отправления, пожалуйста.",
+                           reply_markup=keyboard_cities)
+    bot.register_next_step_handler(msg, get_city_from)
+
+
+@bot.message_handler(content_types=["text"])
+def hello_message_handler(message):
+    hello_options = ["прив", "привет", "здравствуй", "здравствуйте", "хей", "ghbdtn", "hi", "hello"]
+    if message.text.strip().lower() in hello_options:
+        first_order_message(message)
+
+
+def get_city_from(message):
+    if message.text not in order.city_mapping.keys():
+        msg = bot.send_message(message.chat.id, "Данного города нет в списке. Введите, пожалуйста, "
+                                                "правильный город.",
+                               reply_markup=keyboard_cities)
+        bot.register_next_step_handler(msg, get_city_from)
+        return
+    order.city_from = message.text
+    msg = bot.send_message(message.chat.id, "Выберите город прибытия, пожалуйста.",
+                           reply_markup=keyboard_cities)
+    bot.register_next_step_handler(msg, get_city_to)
+
+
+def get_city_to(message):
+    if message.text not in order.city_mapping.keys():
+        msg = bot.send_message(message.chat.id, "Данного города нет в списке. Введите, пожалуйста, "
+                                                "правильный город.",
+                               reply_markup=keyboard_cities)
+        bot.register_next_step_handler(msg, get_city_to)
+        return
+    if order.city_from == message.text:
+        order.city_from = None
+        order.city_to = None
+        msg = bot.send_message(message.chat.id, "Города отправления и прибытия должны различаться!"
+                                                "Выберите город отправления.",
+                               reply_markup=keyboard_cities)
+        bot.register_next_step_handler(msg, get_city_from)
+    else:
+        order.city_to = message.text
+        msg = bot.send_message(message.chat.id, "Введите дату в формате \"дд.мм.гггг\", пожалуйста")
+        bot.register_next_step_handler(msg, get_date)
+
+
+def get_date(message):
+    try:
+        raw_msg = message.text.strip()
+        time.strptime(raw_msg, '%d.%m.%Y')
+        if len(raw_msg) != 10:  # date format 04.04.2020 contain 10 symbols HARDCODE!!!
+            raise ValueError
+    except ValueError:
+        msg = bot.send_message(message.chat.id, "Проверьте правильность введённой даты и введите "
+                                                "подходящую, пожалуйста.\n"
+                                                "P.S. В формате \"дд.мм.гггг\"")
+        bot.register_next_step_handler(msg, get_date)
+        return
+    order.date = message.text
+    response = order.get_info()
+    if not response:
+        response = "Информации по текущей дате в данный момент нет."
+    bot.send_message(message.chat.id, response)
+
+
+def unexpected_text_received():
+    pass
+
+
+if __name__ == '__main__':
+    bot.polling()
---
+++ order.py
@@ -0,0 +1,71 @@
+from bs4 import BeautifulSoup as bs4
+import json
+import requests
+
+
+class Order(object):
+    city_from = None
+    city_to = None
+    date = None
+
+    city_mapping = {"д. Переходы": 16,"71 км": 11, "выезд из д. Крево": 19, "г. Сморгонь": 22,
+                    "д. Байдаки": 6, "д. Белькишки": 25, "д. Василевичи": 48, "д. Гаути": 42,
+                    "д. Дайлидки": 38, "д. Дегенево": 27, "д. Изобелино": 36, "д. Крево  почта": 20,
+                    "д. Крево 2": 64, "д. Кунава": 51, "д. Куцевичи": 30, "д. Лубянка": 43,
+                    "д. Мацканы": 37, "д. Медрики": 40, "д. Милейково": 17, "д. Михалкони": 28,
+                    "д. Михалова": 63, "д. Новоселки": 29, "д. Новоспаск": 53, "д. Ореховка": 46,
+                    "д. Осиновщизна": 45, "д. Подберезь": 33, "д. Раковцы": 18, "д. Светоч": 50,
+                    "д. Свиридовичи": 49, "д. Солы": 41, "д. Сутьково": 52, "д. Тракели": 39,
+                    "д.Богуши": 55, "д.Гарани": 7, "д.Крево 3": 21, "д.Лавский Брод": 15,
+                    "д.Селец": 54, "Лесная сказка": 8, "Минск": 5, "Остр. кольцо": 35,
+                    "Островец": 23, "Ошмяны": 24, "пов. на д. Боруны": 31, "пов. на д. Трайги": 26,
+                    "пов. на Сморг. погран. группа": 44, "трасса М6 Воложинский пост": 65,
+                    "трасса М7 д. Дайнова Большая": 67, "трасса М7 пов. на ст. Воложин": 66}
+
+    def _get_info(self):
+
+        city_from = self.city_mapping.get(self.city_from)
+        city_to = self.city_mapping.get(self.city_to)
+        date = self.date
+        request_url = "http://xn--90aiim0b.xn--80aa3agllaqi6bg.xn--90ais/schedules"
+        parameters = {"station_from_id": 0,
+                      "station_to_id": 0,
+                      "city_from_id": city_from,
+                      "city_to_id": city_to,
+                      "date": date}
+        response = requests.get(request_url, params=parameters)
+        response = json.loads(response.text)
+
+        if response["result"] == "success":
+            soup = bs4(response["html"], "html.parser")
+            uls = soup.findAll('ul', class_="sheduleRow")
+            tours = []
+            for i_ul, ul in enumerate(uls):
+                lis = ul.findAll("li")
+                departure_time = lis[0].text.lstrip().split(' ')[0]
+                arrival_time = lis[1].text.split(' ')[0]
+                free_space = lis[2].text
+                price = lis[3].text.split(' ')[0][:-1]
+                tours.append({"Отправление": departure_time,
+                              "Прибытие": arrival_time,
+                              "Свободно": free_space,
+                              "Цена": price})
+            return tours
+        else:
+            return response["errors"]
+
+    def get_info(self):
+        tours = self._get_info()
+        response = []
+        if isinstance(tours, list):
+            for tour in tours:
+                response.append('')
+                for k, v in tour.items():
+                    response.append(f"{k}: {v}")
+        else:
+            response.append("Ошибка! Надеюсь, это как-нибудь поможет :\\")
+            for k, v in tours.items():
+                v = ','.join(v)
+                response.append(f"{k}: {v}")
+        response = '\n'.join(response)
+        return response
